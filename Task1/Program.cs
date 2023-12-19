5cf1c9bf20f900c38b05c495d918cb1ca2562615
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-12-19 21:30:23 +0300

    сделаны все задания

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nums2 = { { 1, 2, 2, 4 }, { 3, 3, 7, 7 } };
+
+            for (int i = 0; i < nums2.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums2.GetLength(1); j++)
+                {
+                    Console.Write(nums2[i, j] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static int Incremention(int value)
+        {
+            return value++ ;
+        }
+    }
+}
