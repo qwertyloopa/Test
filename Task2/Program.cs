1e6fb58afcf21083c9a8c779c5b565c652cd9950
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-12-19 19:48:01 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите любое слово или предложение");
+            StringHelper sh = new StringHelper(Console.ReadLine());
+            Console.WriteLine(sh.ReverseString());
+
+            Console.ReadKey();
+        }
+
+        public class StringHelper
+        {
+            public string stroka { get; }
+
+            public StringHelper(string Stroka)
+            {
+                stroka = Stroka;
+            }
+
+
+            public string ReverseString() 
+            {
+                char[] ch = stroka.ToCharArray();
+                Array.Reverse(ch);
+                return (new string(ch));
+            }
+
+        }
+    }
+}
